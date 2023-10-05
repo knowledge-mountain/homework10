bca1fe8ac7782dfed42b463a6450babfc25a2ee5
luxoft_vbasanets <viktor.basanets@tesco.com>
2023-10-05 10:54:52 +0300

    Extracted webclient instantiation to config file

---
+++ src/main/java/com/basbase/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.basbase.config;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .codecs(conf -> conf.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
+                        .followRedirect(true)
+                        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                        .responseTimeout(Duration.ofMillis(5000))
+                        .doOnConnected(connection -> connection
+                                .addHandlerLast(new ReadTimeoutHandler(5, TimeUnit.SECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(5, TimeUnit.SECONDS)))
+                )).build();
+    }
+}
